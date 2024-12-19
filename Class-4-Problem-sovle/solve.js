4f1338011edfc09d7604ec12b351aa5a1e1d0d2d
MdTaHin00 <mdtahinhassanjihad@gmail.com>
2024-12-19 19:22:20 +0600

    Add files via upload

---
+++ Class-4-Problem-sovle/solve.js
@@ -0,0 +1,67 @@
+//1
+let maximumNumber=Math.max(2,-3,5,8,5.88);
+console.log(maximumNumber);
+
+//2
+let number2=5;
+if (number2%2==0) {
+console.log("Number is positive");
+} else {
+  console.log("Number is negative");
+}
+
+//3
+let divisibleNumver=22;
+if (divisibleNumver%10 ) {
+  console.log("divisible Number");
+  
+} else {
+  console.log("Not divisible Number");
+}
+//4
+let numbercheck=6;
+if (numbercheck%2==0) {
+  console.log(numbercheck,"even number");
+  
+} else {
+  console.log(numbercheck,"odd number");
+  }
+ //5 
+
+ //6 
+ let temperature=30;
+ if (temperature>=30) {
+  console.log("Temperature is Hot");
+  } if (temperature>=25) {
+  console.log("Temperature is cold");
+  } else {
+  console.log("Temperature is normal");
+  }
+
+  //7
+  let maxvalus=Math.max(384,989,779,344,769)
+  let maxvalus2=Math.max(-222,-333,-111,-340);
+  let maxvalus3=Math.max(23.333,30.4444,87.333,23.444);
+  console.log(maxvalus,maxvalus2,maxvalus3);
+  
+  //8
+  let checkNumber=19;
+  if (checkNumber%2==0) {
+    console.log("Even Number");
+  } else {
+   console.log("Odd Number");
+  }
+
+  //9
+let numOne=10;
+let numTwo=10;
+let total=numOne+numTwo;
+console.log(total>30);
+
+//10
+let old=15;
+if (old>=13&&old<=19) {
+  console.log("Teenager");
+} else {
+ console.log("Not a teenager");
+}
